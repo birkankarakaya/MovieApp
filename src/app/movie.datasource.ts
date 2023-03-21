c95dcadb070187ff54929ed7f63ea1039c503a04
Birkan Karakaya <birkan.karakayaa@gmail.com>
2023-03-21 14:05:12 +0300

    About the cover photos and screening of the movies

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -1,7 +1,7 @@
 import { Movie } from "./movie";
 
 export const Movies: Movie[] = [
-    {id: 1, name: 'Movie 1'},
-    {id: 2, name: 'Movie 2'},
-    {id: 3, name: 'Movie 3'}
+    {id: 1, name: 'Movie 1', description: 'Film 1', imageUrl:'sinema.jpg'},
+    {id: 2, name: 'Movie 2', description: 'Film 2', imageUrl:'WhatsApp Görsel 2023-03-19 saat 12.39.57.jpg'},
+    {id: 3, name: 'Movie 3', description: 'Film 3', imageUrl:'WhatsApp Görsel 2023-03-19 saat 12.39.58.jpg'}
 ];
